cc246f6e870ef0e6821230c1b734449efc7082ec
Makeademic <kaplanwizard@gmail.com>
2021-10-13 12:21:48 -0400

    Add files via upload

---
+++ script0.js
@@ -0,0 +1,15 @@
+function oneAlert(){
+alert("1");
+}
+
+function twoAlert(){
+alert("2");
+}
+
+function threeFunction(){
+  document.getElementById("texttobered").style.color = "red";
+}
+
+function fourFunction(){
+  document.getElementById("texttobered").style.color = "black";
+}
